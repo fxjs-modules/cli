a6dc58355197503d107f235bf30a20f0e436f821
richardo2016 <richardo2016@gmail.com>
2022-10-01 02:48:58 +0800

    build: remove typings from repo, upgrade fib-typify.

--- typings/ctrl.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const exit: (code: number) => void;
-export declare const DEFAULT_ARGS: any[];
-export declare const EOL = "\n";
-export declare const PLATFORM_INFO: string;
--- typings/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { RayTsTypeHelpers } from '@richardo2016/ts-type-helpers';
-import FCli = require('./Cli');
-declare type IGetFCli = (...args: RayTsTypeHelpers.ConstructorParams<typeof FCli>) => FCli;
-declare const getFCli: {
-    (name?: string): FCli;
-    default: IGetFCli;
-};
-export = getFCli;
